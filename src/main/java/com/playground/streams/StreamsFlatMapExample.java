6066ec6b790b80aabc2917eff690ae1b291b23ab
Nithin <nithin2889@gmail.com>
2019-10-02 15:11:18 +0530

    distinct, count and sorted method examples

--- src/main/java/com/playground/streams/StreamsFlatMapExample.java
+++ src/main/java/com/playground/streams/StreamsFlatMapExample.java
@@ -6,15 +6,35 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamsFlatMapExample {
-  // print all the activities in a list
+  // 1. print all the activities in a list
   public static List<String> getStudentActivities() {
     return StudentDatabase.getAllStudents().stream() // Stream<Student>
         .map(Student::getActivities) // Stream<List<String>>
         .flatMap(List::stream) // Stream<String>
+        .distinct() // Stream<String>
+        .collect(Collectors.toList());
+  }
+
+  public static long getStudentActivitiesCount() {
+    return StudentDatabase.getAllStudents().stream() // Stream<Student>
+        .map(Student::getActivities) // Stream<List<String>>
+        .flatMap(List::stream) // Stream<String>
+        .distinct() // Stream<String> with distinct function performed
+        .count();
+  }
+
+  public static List<String> getStudentActivitiesSorted() {
+    return StudentDatabase.getAllStudents().stream() // Stream<Student>
+        .map(Student::getActivities) // Stream<List<String>>
+        .flatMap(List::stream) // Stream<String>
+        .distinct() // Stream<String> with distinct function performed
+        .sorted()
         .collect(Collectors.toList());
   }
 
   public static void main(String[] args) {
     System.out.println("getStudentActivities(): " + getStudentActivities());
+    System.out.println("getStudentActivitiesCount(): " + getStudentActivitiesCount());
+    System.out.println("getStudentActivitiesSorted(): " + getStudentActivitiesSorted());
   }
 }
