7f9e590514d692eb1f149ffa6a79d3226a8fbfd8
Nithin <nithin2889@gmail.com>
2019-10-02 13:13:02 +0530

    example for stream

---
+++ src/main/java/com/playground/streams/StreamsExample.java
@@ -0,0 +1,27 @@
+package com.playground.streams;
+
+import com.playground.data.Student;
+import com.playground.data.StudentDatabase;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StreamsExample {
+
+  static Predicate<Student> studentGradeLevelPredicate = student -> student.getGradeLevel() >= 3;
+  static Predicate<Student> studentGpaPredicate = student -> student.getGpa() >= 3.9;
+
+  public static void main(String[] args) {
+    // 1. Student and their list of activities in a Map.
+    // 2. Want just the students who are in the grade level greater than or equal to 3.
+    Map<String, List<String>> studentMap =
+        StudentDatabase.getAllStudents().stream()
+            .filter(studentGradeLevelPredicate) // Stream<Student>
+            .filter(studentGpaPredicate) // Stream<Student>
+            .collect(
+                Collectors.toMap(
+                    Student::getName, Student::getActivities)); // Map<String, List<String>>
+    System.out.println(studentMap);
+  }
+}
