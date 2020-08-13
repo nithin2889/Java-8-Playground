c06364c465f916e86dacd0770d3f9b928d9295aa
akhilamartia <akhila.martia@gmail.com>
2020-08-13 11:18:12 +0530

    lambda variable example code comments

--- src/main/java/com/playground/constructorreference/LambdaVariableExample2.java
+++ src/main/java/com/playground/constructorreference/LambdaVariableExample2.java
@@ -12,9 +12,14 @@ public class LambdaVariableExample2 {
     int value = 4;
     Consumer<Integer> c1 =
         i -> {
+          // not allowed to modify any local variables that is being referenced in the lambda scope.
           // value++;
           System.out.println(value + i);
         };
+    /*
+      even though you are not modifying the variable within the lambda scope you are still not
+      allowed to modify within the method scope.
+    */
     // value = 6;
     c1.accept(4);
   }
